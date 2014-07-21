ccd851161a89dc5f0a77379ece12b7276406b168
Tom Scott <tubbo@users.noreply.github.com>
2014-07-20 22:20:19 -0400

    Organize code a little better.

--- lib/sartre.rb
+++ lib/sartre.rb
@@ -1,17 +1,23 @@
 require "sartre/version"
 
 module Sartre
-  def method_missing name, *arguments
-    super name, arguments unless name =~ /\?\Z/
-    false unless respond_to? name
+  protected
+  def existential?(name)
+    name =~ /\?\Z/
+  end
+  
+  def try?(name)
     !!send(name)
   end
 end
 
 class Object
+  include Sartre
+  
   def method_missing name, *arguments
-    super name, arguments unless name =~ /\?\Z/
-    return !!send(name[0..-2]) rescue NoMethodError
-    false
+    super unless existential? name
+    method = name[0..-2]
+    return false unless respond_to? method
+    try? method
   end
 end
